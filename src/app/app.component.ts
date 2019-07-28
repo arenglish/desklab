a5ed3dcd7e84c605e9fe981f81fa30bb41701368
arenglish <austinenglish4@gmail.com>
2019-07-27 21:53:56 -0500

    adds main page layout placeholders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    '[style.height]': '"100%"'
+  }
 })
 export class AppComponent {
   title = 'desklab';
